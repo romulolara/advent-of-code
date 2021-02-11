e4786a224816672bbf8356a0c36dc793454dcc1a
romulo.lara <romulo.lara@dataprev.gov.br>
2021-02-11 13:48:58 -0300

    2020 day10 part2 javascript

--- 2020/day10/index.js
+++ 2020/day10/index.js
@@ -40,3 +40,41 @@ console.log({
   diff3Count,
   calcPart1: diff1Count * diff3Count
 });
+
+function findTotalArrangements(firstAdapter = 0, lastAdapter = 0, total = 0) {
+  if (firstAdapter === lastAdapter) {
+    total++;
+  } else if (firstAdapter < lastAdapter) {
+    if (adapters.includes(firstAdapter + 1)) {
+      total += findTotalArrangements(firstAdapter + 1, lastAdapter);
+    }
+    if (adapters.includes(firstAdapter + 2)) {
+      total += findTotalArrangements(firstAdapter + 2, lastAdapter);
+    }
+    if (adapters.includes(firstAdapter + 3)) {
+      total += findTotalArrangements(firstAdapter + 3, lastAdapter);
+    }
+  }
+  return total;
+}
+
+// console.log(diff3Adapters);
+let calcPart2 = 1;
+for (let i = 0; i < diff3Adapters.length; i++) {
+  const startAdapter = i === 0 ? 0 : diff3Adapters[i - 1];
+  const endAdapter = diff3Adapters[i];
+
+  let count = findTotalArrangements(startAdapter, endAdapter);
+  if (count === 0) count = 1;
+  calcPart2 *= count;
+
+  // console.log({
+  //   startAt: startAdapter,
+  //   endAt: endAdapter,
+  //   startIdx: adapters.indexOf(startAdapter),
+  //   endIdx: adapters.indexOf(endAdapter),
+  //   count
+  // });
+}
+
+console.log({ calcPart2 });
