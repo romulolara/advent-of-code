69b11e6db4cd6627de90e971c0c6d2029273824e
romulo.lara <romulo.lara@dataprev.gov.br>
2021-02-14 23:11:36 -0300

    2020 day11 part2 javascript

--- 2020/day11/index.js
+++ 2020/day11/index.js
@@ -14,24 +14,29 @@ const readFile = () => {
 
 const seatsMap = readFile();
 
-function printMap(map, ignore = true) {
-  if (ignore) {
-    return;
+function printMap(map, round, log = null) {
+  if (log === "all" || log === "round") {
+    console.log(round);
   }
-  map.forEach((linha, indice) => {
-    console.log({ [indice]: linha });
-  });
+
+  if (log === "all" || log === "map") {
+    map.forEach((linha, indice) => {
+      console.log({ [indice]: linha });
+    });
+  }
+  return;
 }
 
 function countOccupiedSeats(count, line = "") {
   return count + line.replace(/[^#]/g, "").length;
 }
 
-function changeSeat(seat, adjacents = "") {
+function changeSeat(seat, adjacents = "", newRule = false) {
   if (seat == "L" && adjacents.indexOf("#") < 0) {
     return "#";
   }
-  if (seat === "#" && adjacents.replace(/[^#]/g, "").length >= 4) {
+  let tolerance = newRule ? 5 : 4;
+  if (seat === "#" && adjacents.replace(/[^#]/g, "").length >= tolerance) {
     return "L";
   }
   return seat;
@@ -76,11 +81,12 @@ function updateSeatsMap(map, occupied = 0, rounds = 0) {
     }
     newMap.push(newLine);
   }
-  let newOccupied = newMap.reduce(countOccupiedSeats, 0);
-  console.log({ newOccupied, rounds: ++rounds });
 
-  printMap(newMap);
+  let newOccupied = newMap.reduce(countOccupiedSeats, 0);
+  printMap(newMap, { newOccupied, rounds: ++rounds });
+
   if (occupied === newOccupied) {
+    console.log({ part1Count: newOccupied });
     return;
   } else {
     updateSeatsMap(newMap, newOccupied, rounds);
@@ -88,3 +94,45 @@ function updateSeatsMap(map, occupied = 0, rounds = 0) {
 }
 
 updateSeatsMap(seatsMap);
+
+function findFirstAdjacents(map, l, c) {
+  if (map[l][c] === ".") {
+    return "";
+  }
+  return Object.keys(directions).reduce((total, direction) => {
+    let nextSeat = findNextSeat(map, l, c, direction);
+    let [nextL, nextC] = directions[direction](l, c);
+    while (nextSeat != "" && nextSeat != "L" && nextSeat != "#") {
+      nextSeat = findNextSeat(map, nextL, nextC, direction);
+      [nextL, nextC] = directions[direction](nextL, nextC);
+    }
+    return total + nextSeat;
+  }, "");
+}
+
+function updateSeatsMapPart2(map, occupied = 0, rounds = 0) {
+  const mapLength = map.length;
+  const lineLength = map[0].length;
+  let newMap = [];
+
+  for (let i = 0; i < mapLength; i++) {
+    let newLine = "";
+    for (let j = 0; j < lineLength; j++) {
+      const adjacents = findFirstAdjacents(map, i, j);
+      newLine += changeSeat(map[i][j], adjacents, true);
+    }
+    newMap.push(newLine);
+  }
+
+  let newOccupied = newMap.reduce(countOccupiedSeats, 0);
+  printMap(newMap, { newOccupied, rounds: ++rounds });
+
+  if (occupied === newOccupied) {
+    console.log({ part2Count: newOccupied });
+    return;
+  } else {
+    updateSeatsMapPart2(newMap, newOccupied, rounds);
+  }
+}
+
+updateSeatsMapPart2(seatsMap);
