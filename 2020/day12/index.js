e75f356721aa3c0142b93d95b4f383ef6b79e71d
romulo.lara <romulo.lara@dataprev.gov.br>
2021-02-18 15:38:44 -0300

    2020 day12 part2 javascript

--- 2020/day12/index.js
+++ 2020/day12/index.js
@@ -17,19 +17,6 @@ const navigation = readFile();
 const orientations = ["east", "south", "west", "north"];
 let orientation = orientations[0];
 
-let moves = {
-  east: 0,
-  south: 0,
-  west: 0,
-  north: 0
-};
-
-const instruction = {
-  type: "type",
-  action: "action",
-  value: "value"
-};
-
 function getOrientation(action) {
   if (action === "E") {
     return orientations[0];
@@ -40,44 +27,111 @@ function getOrientation(action) {
   } else if (action === "N") {
     return orientations[3];
   } else {
+    console.log({ erro: action });
     return null;
   }
 }
 
 function convertInstruction(instruction = "") {
-  const [inst, orientation, turn, forward, value] = instruction.match(
+  const [inst, o, turn, forward, value] = instruction.match(
     /([NSEW]?)([LR]?)([F]?)([0-9]+)/
   );
   return {
-    type: orientation ? "O" : turn ? "T" : "F",
-    action: orientation || turn || forward,
+    type: o ? "MOVE" : turn ? "TURN" : "FORWARD",
+    action: o || turn || forward,
     value: Number(value)
   };
 }
 
-function turnOrientation({ action, value }) {
+function turnOrientation(current, { action, value }) {
   const total = orientations.length;
   const steps = value / 90;
-  let next = orientations.indexOf(orientation);
+  let next = orientations.indexOf(current);
   if (action === "R") {
     next = next + steps >= total ? next + steps - total : next + steps;
   } else {
     next = next - steps < 0 ? next - steps + total : next - steps;
   }
-  // console.log({ orientation, next, o: orientations[next] });
-  orientation = orientations[next];
+  return orientations[next];
 }
+
+function moveOrientation(currentMoves, o, { value }) {
+  currentMoves[o] += value;
+  return currentMoves;
+}
+
+function turnWaypoint(currentWaypoint, { action, value }) {
+  let nextWaypoint = [];
+  Object.keys(currentWaypoint).forEach((k) => {
+    nextWaypoint[turnOrientation(k, { action, value })] = currentWaypoint[k];
+  });
+  return nextWaypoint;
+}
+
+function moveShip(currentWaypoint, currentShip, { value }) {
+  Object.keys(currentWaypoint).forEach((k) => {
+    currentShip[k] = currentShip[k] + currentWaypoint[k] * value;
+  });
+  return currentShip;
+}
+
+function moveWaypoint(currentWaypoint, newO, { value }) {
+  if (Object.keys(currentWaypoint).includes(newO)) {
+    currentWaypoint[newO] += value;
+  } else {
+    let inverseO = turnOrientation(newO, { action: "R", value: 180 });
+    let inverseV = currentWaypoint[inverseO] - value;
+
+    if (inverseV < 0) {
+      let newWaypoint = [];
+      Object.keys(currentWaypoint).forEach((k) => {
+        if (k === inverseO) {
+          newWaypoint[newO] = Math.abs(inverseV);
+        } else {
+          newWaypoint[k] = currentWaypoint[k];
+        }
+      });
+      return newWaypoint;
+    } else {
+      currentWaypoint[inverseO] = inverseV;
+    }
+  }
+  return currentWaypoint;
+}
+
+let moves = {
+  east: 0,
+  south: 0,
+  west: 0,
+  north: 0
+};
+
+let ship = {
+  east: 0,
+  south: 0,
+  west: 0,
+  north: 0
+};
+
+let waypoint = {
+  east: 10,
+  north: 1
+};
 
 function navigate(instruction) {
   if (instruction) {
     const { type, action, value } = convertInstruction(instruction);
-    console.log({ instruction, type, action, value });
-    if (type === "F") {
-      moves[orientation] += value;
-    } else if (type === "O") {
-      moves[getOrientation(action)] += value;
-    } else if (type === "T") {
-      turnOrientation({ action, value });
+    // console.log(instruction, { type, action, value });
+    if (type === "FORWARD") {
+      moves = moveOrientation(moves, orientation, { value });
+      ship = moveShip(waypoint, ship, { value });
+    } else if (type === "MOVE") {
+      const newO = getOrientation(action);
+      moves = moveOrientation(moves, newO, { value });
+      waypoint = moveWaypoint(waypoint, newO, { value });
+    } else if (type === "TURN") {
+      orientation = turnOrientation(orientation, { action, value });
+      waypoint = turnWaypoint(waypoint, { action, value });
     }
   }
 }
@@ -86,12 +140,13 @@ for (const i of navigation) {
   navigate(i);
 }
 
-let ew = Math.abs(moves.east - moves.west);
-let ns = Math.abs(moves.north - moves.south);
-
 console.log({
   orientation,
-  ew,
-  ns,
-  distance: ew + ns
+  moves,
+  distanceP1:
+    Math.abs(moves.east - moves.west) + Math.abs(moves.north - moves.south),
+  ship,
+  waypoint,
+  distanceP2:
+    Math.abs(ship.east - ship.west) + Math.abs(ship.north - ship.south)
 });
